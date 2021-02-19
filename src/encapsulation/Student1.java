62807ee01ef5c7dd76d770b396a8242faf367301
Prathmesh <prathmeshdeo13@gmail.com>
2021-02-20 01:54:00 +0530

    Student1

---
+++ src/encapsulation/Student1.java
@@ -0,0 +1,46 @@
+package encapsulation;
+
+public class Student1 {
+    private int Studentid;
+    private String Studentname;
+    private String Studentadd;
+
+    public int getStudentid() {
+        return Studentid;
+    }
+
+    public void setStudentid(int studentid) {
+        Studentid = studentid;
+    }
+
+    public String getStudentname() {
+        return Studentname;
+    }
+
+    public void setStudentname(String studentname) {
+        Studentname = studentname;
+    }
+
+    public String getStudentadd() {
+        return Studentadd;
+    }
+
+    public void setStudentadd(String studentadd) {
+        Studentadd = studentadd;
+    }
+
+    public static void main(String[] args) {
+        Student1 s1 = new Student1();
+        s1.setStudentid(23);
+        s1.setStudentname("Rohan");
+        s1.setStudentadd("Pune");
+        System.out.println(s1.getStudentid());
+        System.out.println(s1.getStudentname());
+        System.out.println(s1.getStudentadd());
+
+
+
+    }
+}
+
+
