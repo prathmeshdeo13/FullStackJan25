98151473cbac12cf5af9638c76bc9a66ebf8887f
Prathmesh <prathmeshdeo13@gmail.com>
2021-08-07 17:12:48 +0530

    StringBuffer

---
+++ src/string/StringBuffer1Demo.java
@@ -0,0 +1,10 @@
+package string;
+
+public class StringBuffer1Demo {
+    public static void main(String[] args) {
+
+        StringBuffer sb = new StringBuffer("Prathamesh");
+        sb.append("Shekhar java");
+        System.out.println(sb.capacity());
+    }
+}
--- src/string/stringConcanetingMethod/subSequenceMethod.java
+++ src/string/stringConcanetingMethod/subSequenceMethod.java
@@ -1,7 +1,7 @@
 package string.stringConcanetingMethod;
 
 public class subSequenceMethod {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         String s = "this is demo";
         System.out.println(s.subSequence(3,9));
         System.out.println(s.substring(1));
